15c86f106fb42295d0fc0d64bf42d946f5a7a01a
Matthew Evans <mevans+git@wtfsystems.net>
2021-10-03 21:32:17 -0400

    sync

--- src/WtGui.js
+++ src/WtGui.js
@@ -2,20 +2,45 @@
  *
  */
 
+import Gamepads from 'gamepads'
+
+Gamepads.start()
+
+/*
+ *
+ */
 export const WtGuiConfig = {
     //
 }
 
+/*
+ *
+ */
 export class WtGuiMenu {}
 
+/*
+ *
+ */
 class WtGuiItem {}
 
+/*
+ *
+ */
 export class WtGuiButton extends WtGuiItem {}
 
+/*
+ *
+ */
 export class WtGuiLabel extends WtGuiItem {}
 
+/*
+ *
+ */
 export class WtGuiInput extends WtGuiItem {}
 
+/*
+ *
+ */
 export default (
     WtGuiConfig,
     WtGuiMenu,
