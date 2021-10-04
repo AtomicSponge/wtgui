b66c8ba2f4c40fee995505460d6aecb52e63e27e
Matthew Evans <mevans+git@wtfsystems.net>
2021-10-03 21:50:20 -0400

    sync

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+/*
+ *
+ */
+
+const { app, dialog, ipcMain, Tray, Menu, MenuItem, BrowserWindow } = require('electron')
+
 import { WtGuiConfig } from "./src/WtGui"
 
 WtGuiConfig.canvas = 'set'
+
+let mainWindow = null
+const mainWindow = () => {
+	settingsWin = new BrowserWindow({
+		title: `TESTING`,
+		width: 1024,
+		height: 768,
+		fullscreen: false,
+		fullscreenable: true,
+		autoHideMenuBar: true,
+		webPreferences: {
+			contextIsolation: true,
+			nativeWindowOpen: true,
+			preload: path.join(__dirname, '_wtguitest.js')
+		}
+	})
+	settingsWin.on('close', (event) => {
+		settingsWin.destroy()
+	})
+	settingsWin.webContents.on('dom-ready', () => {
+		settingsWin.webContents.send('send-json-data', data)
+	})
+	settingsWin.loadFile('_wtguitest.html')
+}
