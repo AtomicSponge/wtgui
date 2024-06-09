f53b7cfb09b6825e5c463ba72511ab426ed319e8
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-08 23:06:06 -0400

    sync

--- src/routes/MainMenu.vue
+++ src/routes/MainMenu.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goToTestA = () => {
+  router.push('/testa')
+}
+
+const goToTestB = () => {
+  router.push('/testb')
+}
+
 </script>
 
 <template>
   <h1>Main Menu</h1>
+  <button @click="goToTestA">Test Menu A</button>
+  <button @click="goToTestB">Test Menu B</button>
 </template>
 
 <style lang="stylus" scoped>
--- src/routes/TestMenuB.vue
+++ src/routes/TestMenuB.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goToMain = () => {
+  router.push('/')
+}
+
 </script>
 
 <template>
   <h1>Test Menu B</h1>
+  <button @click="goToMain">Main Menu</button>
 </template>
 
 <style lang="stylus" scoped>
