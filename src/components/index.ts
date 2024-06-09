a740357c63283cb41b519955c08205e71abea92d
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-08 20:01:13 -0400

    sync

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @author Matthew Evans
+ * @module wtfsystems/wtgui
+ * @see README.md
+ * @copyright MIT see LICENSE.md
+ */
+
+export { default as HelloWorld } from './HelloWorld.vue'
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
+const LIBRARY_NAME = 'wtgui'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), cssInjectedByJsPlugin()
+  ],
+  build: {
+    cssCodeSplit: true,
+    target: 'esnext',
+    outDir: './dist',
+    minify: true,
+    lib: {
+      entry: './src/components/index.ts',
+      name: LIBRARY_NAME,
+      fileName: LIBRARY_NAME
+    },
+    rollupOptions: {
+      external: [ 'vue' ],
+      output: { 
+        exports: 'named',
+        globals: { vue: 'Vue' }
+      }
+    }
+  }
 })
