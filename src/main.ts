a08dda65f17c2b666a3dcd7907faff3ff3ae4baf
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-09 08:56:24 -0400

    sync

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router.ts'
 import './style.styl'
 
+const pinia = createPinia()
+
 createApp(App)
   .use(router)
+  .use(pinia)
   .mount('#app')
