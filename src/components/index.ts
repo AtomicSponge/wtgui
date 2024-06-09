6b447eb4692202b0939f7f61b122a06a45eece73
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-09 10:39:54 -0400

    sync

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,8 @@
  * @copyright MIT see LICENSE.md
  */
 
+export { default as WtguiMenu } from './WtguiMenu.vue'
+
 export { default as WTGuiButton } from './WTGuiButton.vue'
 export { default as WTGuiInputSetting } from './WTGuiInputSetting.vue'
 export { default as WTGuiSelect } from './WTGuiSelect.vue'
