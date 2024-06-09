93cf59d90b9df084864a0dd486fff8716cc21d50
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-09 10:31:30 -0400

    sync

---
+++ src/components/WtguiMenu.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+//defineProps<{}>()
+</script>
+
+<template>
+  <section>
+    <slot></slot>
+  </section>
+</template>
+
+<style lang="stylus" scoped>
+section
+  border 1px solid red
+</style>
--- src/routes/MainMenu.vue
+++ src/routes/MainMenu.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 
+import WtguiMenu from '../components/WtguiMenu.vue'
+
 const router = useRouter()
 
 const goToTestA = () => {
@@ -14,9 +16,11 @@ const goToTestB = () => {
 </script>
 
 <template>
-  <h1>Main Menu</h1>
-  <button @click="goToTestA">Test Menu A</button>
-  <button @click="goToTestB">Test Menu B</button>
+  <wtgui-menu>
+    <h1>Main Menu</h1>
+    <button @click="goToTestA">Test Menu A</button>
+    <button @click="goToTestB">Test Menu B</button>
+  </wtgui-menu>
 </template>
 
 <style lang="stylus" scoped>
