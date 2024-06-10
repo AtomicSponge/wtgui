db1874175f0f14061da7e83313b8a93c4c7d4ab1
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-10 11:09:10 -0400

    sync

---
+++ src/components/WTGuiMessageBox.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  msg: string
+}>()
+
+const modalStyle = ref('display: hidden;')
+</script>
+
+<template>
+  <div :style="modalStyle"></div>
+</template>
+
+<style lang="stylus" scoped>
+</style>
