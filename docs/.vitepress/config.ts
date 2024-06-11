81d6c276a765a005ae76dabb3c5f646e5c4c3435
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-11 19:41:59 -0400

    sync

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  outDir: './.vitepress/docs/wtgui',
+  title: "WTGui Documentation",
+  description: "Experimental VueJS Menu System",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
