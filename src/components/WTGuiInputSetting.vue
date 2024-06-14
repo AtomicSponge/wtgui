2bcf01e5d1350908c0c461732b3e7372a3efcadf
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-14 10:53:04 -0400

    sync

--- src/components/WTGuiInputSetting.vue
+++ src/components/WTGuiInputSetting.vue
@@ -83,7 +83,7 @@ const doInput = (event:any):void => {
 const captureKey = (event:any):void => {
   if(showInputMessageBox.value && event.type === 'keydown') {
     window.removeEventListener('keydown', captureKey)
-    settingValue.value = event.key
+    settingValue.value = event.key  
   }
 }
 
