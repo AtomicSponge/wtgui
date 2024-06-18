dc20d7f59e4143241e364dc533fe4d6cc68c63b8
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-18 17:51:18 -0400

    sync

--- src/lib/generateId.ts
+++ src/lib/generateId.ts
@@ -1,6 +1,6 @@
 /**
- * Creates a random number with 20 digits
- * @returns A random number
+ * Creates a random number for use in Ids
+ * @returns A random number as a string
  */
 export const generateId = ():string => {
   return `${Math.floor(Math.pow(10, 20) + Math.random() * (Math.pow(10, 20) - Math.pow(10, 20 - 1) - 1))}`
