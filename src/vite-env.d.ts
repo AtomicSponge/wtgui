c695fd37108ed0ecd0cc0117c33630ec9a38d1e3
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-20 18:34:47 -0400

    sync

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,18 @@
 /// <reference types="vite/client" />
 
+/** Options for WTGui */
 interface WTGuiOptions {
+  /** Game title */
   gameTitle:string
+  /** Fonst style in CSS */
   fontStyle?:string
+  /** Title color in CSS */
   titleColor?:string
+  /** Border color in CSS */
   borderColor?:string
+  /** Item color in CSS */
   itemColor?:string
+  /** Focus color in CSS */
   focusColor?:string
 }
 
