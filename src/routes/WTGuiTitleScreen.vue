45d8dfebe654cfb45aea0671673509aeaba96ff2
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-23 10:19:41 -0400

    sync

--- src/routes/WTGuiTitleScreen.vue
+++ src/routes/WTGuiTitleScreen.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { inject, onMounted, onBeforeUnmount } from 'vue'
 import { useRouter } from 'vue-router'
+import { useGamepad } from '@vueuse/core'
 import WtguiMenu from '../components/WtguiMenu.vue'
 import WTGuiLabel from '../components/WTGuiLabel.vue'
 
 const router = useRouter()
+
+const { onConnected } = useGamepad()
 
 /** Main menu route provided from options */
 const mainMenuRoute = <string>inject('mainMenuRoute')
@@ -13,18 +16,22 @@ const gameTitle = <string>inject('gameTitle')
 /** Game title scale provided from options */
 const defaultScale = <number>inject('defaultScale')
 
+/** Delay for menu transition */
 const delay:MenuDelay = 300
 
 /**
  * Go to the main menu on input
  * @param event Input event
  */
-const goToMain = (event:any) => {
-  event.preventDefault()
+const goToMain = (event?:any) => {
+  if(event !== undefined) event.preventDefault()
   setTimeout(() => {
     router.push(mainMenuRoute)
   }, delay)
 }
+
+//  Gamepad was interacted with
+onConnected(() => { goToMain() })
 
 onMounted(() => {
   window.addEventListener('keydown', goToMain)
